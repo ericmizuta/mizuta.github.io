1b1d24a412057deb3b39790bfa7f6579c275cfe6
Mizuta <mizutaeric@gmail.com>
2022-05-15 20:59:40 -0300

    add page title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Mizuta.dev | Home</title>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
